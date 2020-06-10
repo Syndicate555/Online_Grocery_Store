dd17fbda868dbcb16d1fe6c3b7861f6e43838ab2
Syndicate555 <saziz028@uottawa.ca>
2020-06-10 04:09:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-// variables
+// DOM elements
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart");
-const cartOverlay = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const productsDOM = document.querySelector(".products-center");
